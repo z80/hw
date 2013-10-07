48a64c22355d7a01a8b09410fc82dbdb67bd3c37
saasbook <saasbook@saasbook.(none)>
2013-10-07 00:43:30 -0700

    Part two.

--- hw01/hw02.rb
+++ hw01/hw02.rb
@@ -1,7 +1,44 @@
 
+def hello( name )
+  return "Hello, " + name
+end
+
+def starts_with_consonant?( s )
+  if (s.length < 1)
+    return false
+  end
+  res = s.match( /^[^AEIOU].+$/i )
+  if ( !res )
+    return false
+  end
+  if ( res.size() > 0 )
+      return true
+  end
+  return false
+end
+
+def binary_multiple_of_4?( s )
+  res = s.match( /[01]*/ )
+  if ( !res )
+    return false
+  end
+  res = s.match( /^[01]*00$/ )
+  if ( !res )
+    return false
+  end
+  if ( res.size() > 0 )
+    return true
+  end
+  return false
+end
 
 
 
-
-
+# tests ....
+#puts "Tests: "
+#puts hello( "dasda" )
+#puts "Consonant test: "
+#puts starts_with_consonant?( "bBfds" )
+#puts "binary multiple of 4 test: "
+#puts binary_multiple_of_4?( "0011100" )
 
