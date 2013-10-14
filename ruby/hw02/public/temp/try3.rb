e8e3c8735eed9bf767d1a96a12c4b345acdf76d5
z80 <bashkirov.sergey@gmail.com>
2013-10-14 16:24:21 +0400

    Changed a bit.

--- ruby/hw02/public/temp/try3.rb
+++ ruby/hw02/public/temp/try3.rb
@@ -3,8 +3,9 @@ module SpaceShip
 
     def <=>( b )
         a = self
+        # Euqal strategy means first wins by definition.
         if ( a == b )
-            return 0
+            return 1
         end
         if ( a == "r" )
             if ( b == "s" )
@@ -29,9 +30,10 @@ module SpaceShip
             if ( b == "r" )
                 return 1
             end
-	end
-	
-	return -1
+            
+        end
+    
+        return -1
     end
 
 end
@@ -55,20 +57,32 @@ class RockPaperScissors
             return player2
         end
 
-        def self.tournament_winner(tournament)
+        def self.tournament_winner( tournament )
             # YOUR CODE HERE
+            s1 = tournament[0]
+            s2 = tournament[1]
+            # check if both are final candidates.
+            if ( ( s1[0].is_a? String ) && ( s2[0].is_a? String ) )
+                return winner( s1, s2 )
+            end
+            # Else these are branches of tournament.
+            # It's necessary to iterate the same function th them both 
+            # And take a winner afterwards.
+            s1 = tournament_winner( s1 )
+            s2 = tournament_winner( s2 )
+            return winner( s1, s2 )
         end
       
         def self.winner_index( s1, s2 )
             a = [ "r", "p", "s" ]
             i1 = a.index( s1 )
             if ( i1 == nil )
-		puts s1
+                puts s1
                 raise NoSuchStrategyError
             end
             i2 = a.index( s2 )
             if ( i2 == nil )
-		puts s2
+                puts s2
                 raise NoSuchStrategyError
             end
             if ( s1 >= s2 )
@@ -82,6 +96,7 @@ end
 
 
 puts RockPaperScissors.winner( [ "aaa", "p" ], [ "vvv", "s" ] )
+
 
 
 
