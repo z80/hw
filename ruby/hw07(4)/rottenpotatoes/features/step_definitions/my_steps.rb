2cf91817f8eab01268a227c6b2a1e2271d002e3f
z80 <bashkirov.sergey@gmail.com>
2013-11-05 23:49:31 +0400

    Started step definitions.

---
+++ ruby/hw07(4)/rottenpotatoes/features/step_definitions/my_steps.rb
@@ -0,0 +1,14 @@
+
+Given(/^the following movies exist:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the director of "(.*?)" should be "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+i
+
+
+
+
