1d368078372ebce445077d66792852d0864ca3c9
z80 <bashkirov.sergey@gmail.com>
2015-08-02 11:59:52 +0300

    a

---
+++ javascript/17/script17.js
@@ -0,0 +1,13 @@
+$(function() {
+  //alert('got here');
+  $("#tabs").tabs().tabs( "add",
+                          "./C9JS_16.html",
+                          "Click-a-bob" );
+
+
+  $("#datepicker").datepicker({
+    onSelect: function(dateText, inst) {
+      $('#title').text("You picked: " + dateText);
+    }
+  });
+});
